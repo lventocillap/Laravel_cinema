0b7acbec83b262d6c950de8303401cdf881b9017
Luis Fernando Ventocilla <1507430@senati.pe>
2024-10-01 14:27:09 -0500

    feat: :sparkles: Check out Billboard Movie
    Verifica la pelicaula de Billboard si esta disponible o no

--- app/Http/Middleware/MovieAthentication.php
+++ app/Http/Middleware/MovieAthentication.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Middleware;
 
+use App\Enums\MovieStatusesEnum;
+use App\Models\Billboard;
 use Closure;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class MovieAthentication
@@ -15,6 +20,14 @@ class MovieAthentication
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $billboardID = $request->billboard_id;
+        $billboard = Billboard::find($billboardID);
+        if (!$billboard) {
+            return throw new Exception('Not found Billboard', HttpResponse::HTTP_NOT_FOUND);
+        }
+        if($billboard->movie->movieStatus->name !== MovieStatusesEnum::Cartelera->value) {
+            return new JsonResponse(['message'=>'Movie not avalaible']);
+        }
         return $next($request);
     }
 }
