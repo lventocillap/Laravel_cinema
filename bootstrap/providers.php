8819b6dbd5332a8958d5f2cf06e7b40fd86c888d
Luis Fernando Ventocilla <1507430@senati.pe>
2024-10-02 16:42:54 -0500

    feat: :sparkles: Add proveders in the DetailSale, Sale and Seat
    Se agrego los services providers de los constextos mencionados

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    Src\Movie\Infrastructure\MovieServiceProvider::class
+    Src\Movie\Infrastructure\MovieServiceProvider::class,
+    Src\DetailSale\Infrastructure\DetailSaleServiceProvider::class,
+    Src\Sale\Infrastructure\SaleServiceProvider::class,
+    Src\Seat\Infrastructure\SeatServiceProvider::class
 ];
