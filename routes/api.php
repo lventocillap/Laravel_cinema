5c75123fa80df9772074fd5ab565011ad8293cd3
Luis Fernando Ventocilla <1507430@senati.pe>
2024-10-12 16:53:49 -0500

    feat: :sparkles: New Route Image
    Nueva ruta para Image

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,14 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DetailSaleController;
+use App\Http\Controllers\ImageController;
 use App\Http\Controllers\MovieController;
 use App\Http\Middleware\Authentication;
 use App\Http\Middleware\DateMovieMiddleware;
 use App\Http\Middleware\MovieAthentication;
 use App\Http\Middleware\MovieMiddlware;
 use App\Http\Middleware\SaleUserMiddleware;
+use App\Http\Middleware\SeatSaleAuthentication;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Src\DetailSale\Infrastructure\Controller\DetailSaleController as SrcDetailSaleController;
@@ -35,17 +37,24 @@ Route::group([
     Route::get('/{id}','show');
     Route::post('/','store');
 });
-
 Route::group([
     'prefix' => 'login',
     'controller' => AuthController::class,
 ], static function(){
     Route::post('/', 'login')->withoutMiddleware(Authentication::class);
 });
-
 Route::group([
     'prefix' => 'sale',
     'controller' => SrcDetailSaleController::class,
 ], static function(){
-    Route::post('/', 'newDetailSale');
+    Route::post('/', 'newDetailSale')->middleware([MovieAthentication::class,SeatSaleAuthentication::class]);
+});
+Route::group([
+    'prefix' => 'image',
+    'controller' => ImageController::class,
+], static function(){
+    Route::post('/movie/{movie}','storeImageMovie');
+    Route::post('/user/{user}','storeImageUser');
+    Route::get('/movie/{movie}','getImageMovie');
+    Route::get('/user/{user}','getImageUser');
 });
