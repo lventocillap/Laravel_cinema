fa50fb65f277043644bf22b7df46c372b724df7a
Luis Fernando Ventocilla <1507430@senati.pe>
2024-10-12 16:40:00 -0500

    feat: :sparkles: New Transaccion
    Se agrego transaccion a la creacion de venta

--- Src/DetailSale/Application/UseCase/NewDetailSale.php
+++ Src/DetailSale/Application/UseCase/NewDetailSale.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Src\DetailSale\Application\UseCase;
 
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Src\DetailSale\Application\DTO\DetailSaleRequest;
 use Src\DetailSale\Domain\Interface\DetailSaleInterface;
 
@@ -16,7 +18,7 @@ class NewDetailSale
     public function execute(DetailSaleRequest $request):void
     {
         if(count($request->priceIds) !== count($request->seatIds)){
-            
+            throw new Exception('They are not the same');
         }
         foreach($request->priceIds as $index => $priceId)
         $this->detailSaleInterface->newDetailSale(
--- Src/DetailSale/Infrastructure/Controller/DetailSaleController.php
+++ Src/DetailSale/Infrastructure/Controller/DetailSaleController.php
@@ -1,10 +1,12 @@
-<?php 
+<?php
+
 declare(strict_types=1);
 
 namespace Src\DetailSale\Infrastructure\Controller;
 
 use App\Http\Requests\DetailSale\DetailSaleRequest as AppDetailSaleRequest;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 use Src\DetailSale\Application\DTO\DetailSaleRequest;
 use Src\DetailSale\Application\UseCase\NewDetailSale;
 use Src\Sale\Application\UseCase\CreateNewSale;
@@ -16,20 +18,21 @@ class DetailSaleController
         private NewDetailSale $newDetailSale,
         private CreateNewSale $createNewSale,
         private UpdatesSeatsStatus $updatesSeatsStatus
-    )
-    {}
-    public function newDetailSale(APPDetailSaleRequest $request):JsonResponse
+    ) {}
+    public function newDetailSale(APPDetailSaleRequest $request): void
     {
-        $sale = $this->createNewSale->execute($request->price_id);
-        $this->newDetailSale->execute(new DetailSaleRequest(
-            $sale->getId(),
-            $request->billboard_id,
-            $request->seat_id,
-            $request->price_id
-        ));
-        $this->updatesSeatsStatus->execute($request->seat_id);
-        return new JsonResponse([
-            'message' => 'create new Sale'
-        ]);
+        DB::transaction(function () use ($request) {
+            $sale = $this->createNewSale->execute($request->price_id);
+            $this->newDetailSale->execute(new DetailSaleRequest(
+                $sale->getId(),
+                $request->billboard_id,
+                $request->seat_id,
+                $request->price_id
+            ));
+            $this->updatesSeatsStatus->execute($request->seat_id);
+            return new JsonResponse([
+                'message' => 'create new Sale'
+            ]);
+        });
     }
 }
