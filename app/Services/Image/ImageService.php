6b7700c39cbcce81ce7a2a2202b38b475d76e83c
Luis Fernando Ventocilla <1507430@senati.pe>
2024-10-16 18:45:36 -0500

    feat: :sparkles: Service SaveBase64 and get extension image
    Funciones para guardar en base 64 y obtener la extension de esa imagen

---
+++ app/Services/Image/ImageService.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services\Image;
+
+use Exception;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class ImageService
+{
+    public function saveBase64(string $base64Image, string $folder = 'movie'): string
+    {
+        $image = base64_decode(explode(',',$base64Image)[1]);
+        $fileExtension = $this->getFileExtension($base64Image);
+        $filename = Str::uuid() . $fileExtension;
+        $path = $folder . '/' . $filename;
+        Storage::disk('public')->put($path, $image);
+        return $path;
+    }
+    public function getFileExtension(string $base64Image): string
+    {
+        $matches = [];
+        if(preg_match('/data:image\/(?<type>.+);base64,/',$base64Image, $matches)){
+            $mineType = $matches['type'];
+            switch($mineType){
+                case 'jpg':
+                    return 'jpg';
+                case 'png':
+                    return 'png';
+                case 'webp':
+                    return 'webp';
+                case 'gif':
+                    return 'gif';
+                default:
+                    throw new Exception('Not extension');
+            }
+        }
+    }
+}
