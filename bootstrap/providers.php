fb4bdb90f90cb4f625087653e574f98bad9ce621
Luis Fernando Ventocilla <1507430@senati.pe>
2024-10-22 21:20:29 -0500

    refactor: :recycle: Order in the declaration of dependencies

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Src\Seat\Infrastructure\SeatServiceProvider::class,
+    Src\Sale\Infrastructure\SaleServiceProvider::class,
     Src\Movie\Infrastructure\MovieServiceProvider::class,
     Src\DetailSale\Infrastructure\DetailSaleServiceProvider::class,
-    Src\Sale\Infrastructure\SaleServiceProvider::class,
-    Src\Seat\Infrastructure\SeatServiceProvider::class
+    Src\Profile\Infrastructure\ProfileServiceProvider::class,
 ];
