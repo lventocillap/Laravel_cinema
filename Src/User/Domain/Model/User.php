df82f38003330f70a4d7493c707dc69032485082
Luis Fernando Ventocilla <1507430@senati.pe>
2024-10-22 21:31:26 -0500

    feat: :sparkles: New Model User of my domain
    Nuevo modelo User de mi domio

---
+++ Src/User/Domain/Model/User.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\User\Domain\Model;
+
+class User
+{
+    public function __construct(
+        private int $id,
+        private string $email,
+        private string $password
+    ) {}
+    public function getId():int
+    {
+        return $this->id;
+    }
+    public function getEmail():string
+    {
+        return $this->email;
+    }
+    public function getPassword():string
+    {
+        return $this->password;
+    }
+}
