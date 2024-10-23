5d1b252490276afe6b31d7fc6334d2d2fccbdf39
Luis Fernando Ventocilla <1507430@senati.pe>
2024-10-22 21:51:02 -0500

    refactor: :recycle: Client parameter added
    Recibe como parametro el ID del cliente sea autentificado o sin autentifacion de usuario

--- Src/Sale/Infrastructure/Persistence/SaleEloquentPersistence.php
+++ Src/Sale/Infrastructure/Persistence/SaleEloquentPersistence.php
@@ -1,33 +1,33 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Src\Sale\Infrastructure\Persistence;
 
 use App\Models\Price;
-use App\Models\Sale as AppSale;
-use Illuminate\Support\Facades\Auth;
-use Src\Sale\Domain\Interface\SaleInterface;
 use Src\Sale\Domain\Model\Sale;
+use App\Models\Sale as AppSale;
+use Src\Sale\Domain\Interface\SaleInterface;
 
 class SaleEloquentPersistence implements SaleInterface
 {
-    public function newSale(array $priceIds): Sale
+    public function newSale(array $priceIds, int $clientId): Sale
     {
         $total = 0;
-        foreach($priceIds as $priceId){
+        foreach ($priceIds as $priceId) {
             $price = Price::find($priceId);
-            if($price){
+            if ($price) {
                 $total += $price->price;
             }
         }
         $sale = AppSale::create([
-            'user_id' => Auth::user()->id,
+            'client_id' => $clientId,
             'date' => now()->format('Y-m-d'),
             'total_price' => $total
         ]);
         return new Sale(
             $sale->id,
-            $sale->user_id,
+            $sale->client_id,
             $sale->date,
             $sale->total_price
         );
