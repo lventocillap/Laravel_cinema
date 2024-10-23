2239583294f0ab3a8064f35e07fa0bfca5bd2504
Luis Fernando Ventocilla <1507430@senati.pe>
2024-10-22 21:48:43 -0500

    refactor: :recycle: Request implementation
    Implemtacion del request que valida los asientos

--- Src/Seat/Application/UseCase/UpdatesSeatsStatus.php
+++ Src/Seat/Application/UseCase/UpdatesSeatsStatus.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Src\Seat\Application\UseCase;
 
+use Src\Seat\Application\DTO\SeatAvalaibleRequest;
 use Src\Seat\Domain\Interface\SeatInterface;
 
 class UpdatesSeatsStatus
@@ -11,9 +12,9 @@ class UpdatesSeatsStatus
     public function __construct(
         private SeatInterface $seatInterface
     ) {}
-    public function execute(array $seatIds):void
+    public function execute(SeatAvalaibleRequest $seatAvalaibleRequest):void
     {
-        foreach($seatIds as $seatId){
+        foreach($seatAvalaibleRequest->seatIds as $seatId){
             $this->seatInterface->updateStatusSeat($seatId);
         }
         
